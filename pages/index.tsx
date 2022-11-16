54443734aa1689f3dc7bd61be46491872e0b5bad
Muhammad Syarif H <42500655+msyrh@users.noreply.github.com>
2022-11-16 15:38:24 +0700

    refactor: delete comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,25 +4,6 @@ import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma  from "../lib/prisma"
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const feed = [
-//     {
-//       id: "1",
-//       title: "Prisma is the perfect ORM for Next.js",
-//       content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-//       published: false,
-//       author: {
-//         name: "Nikolas Burk",
-//         email: "burk@prisma.io",
-//       },
-//     },
-//   ]
-//   return { 
-//     props: { feed }, 
-//     revalidate: 10 
-//   }
-// }
-
 export const getStaticProps:GetStaticProps =async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
